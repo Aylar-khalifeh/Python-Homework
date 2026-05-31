3fb2cdb8aa9468d491abb030b5a64e5ce8633e64
Aylar-khalifeh <aylarrkha@gmail.com>
2026-06-01 02:53:24 +0330

    Add files via upload

---
+++ tamrin1.py
@@ -0,0 +1,135 @@
+
+#فهرست حدس
+# guests=["Ali","Reza","Arash"]
+# print(guests[0] + " " +  "you are invited to the party")
+# print(guests[1] + " " +  "you are invited to the party")
+# print(guests[2] + " " +  "you are invited to the party")
+
+#تغییر فهرست مهمان ها
+# guests=["Ali","Reza","Arash"]
+# print(guests[2] + " " + "cannot attend the party"
+# guests[2]="mohsen"
+# print(guests)
+
+# print(guests[0] + " " + "you are invited to the party")
+# print(guests[1] + " " + "you are invited to the party")
+# print(guests[2] + " " + "you are invited to the party")
+
+#مهمان های بیشتر
+# guests=["Ali","Reza","Arash"]
+# guests.append("Sara")
+# guests.append("Zahra")
+# guests.append("Nima")
+# print(guests)
+
+# print(guests[0] + " " + "you are invited to the party")
+# print(guests[1] + " " + "you are invited to the party")
+# print(guests[2] + " " + "you are invited to the party")
+# print(guests[3] + " " + "you are invited to the party")
+# print(guests[4] + " " + "you are invited to the party")
+# print(guests[5] + " " + "you are invited to the party")
+
+# print("we found a bigger table  for dinner")
+# guests.insert(0,"Arezo")
+# guests.insert(5,"Kamal")
+# guests.append("Ahmad")
+# print(guests)
+
+
+#کوچک کردن فهرست مهمان ها
+# print("unfortunately, the new dinner table won't arrive in time , and we can invite only two people")
+# guests2=guests.pop(1)
+# print(f"you are not invited to the party", guests2)
+# guests2=guests.pop(5)
+# print(f"you are not invited to the party", guests2)
+# guests2=guests.pop(4)
+# print(f"you are not invited to the party", guests2)
+# guests2=guests.pop(2)
+# print(f"you are not invited to the party", guests2)
+# guests2=guests.pop(0)
+# print(f"you are not invited to the party", guests2)
+# guests2=guests.pop(3)
+# print(f"you are not invited to the party", guests2)
+# guests2=guests.pop(0)
+# print(f"you are not invited to the party", guests2)
+# print(guests)
+
+
+#نامه به دعوت شدگان
+# print(f"you are invited to the party",guests[0])
+# print(f"you are invited to the party",guests[1])
+
+#حذف مجدد دعوت شدگان
+# del guests[0]
+# del guests[0]
+# print(guests)
+
+
+#مشاهده جهان
+
+# country=['Iran','Paris','USA','London','Italy']
+# #نمایش فهرست
+# print(country)
+
+# #نمایش فهرست مرتب شده
+# country2=(sorted(country))
+# print(country2)
+
+# #نمایش مجدد فهرست
+# print(country)
+
+# #نمایش عکس لبست
+# country.reverse()
+# print(country)
+
+# #مرتب سازی دایمی
+# country.sort()
+# print(country)
+
+# #مرتب سازی دایمی برعکس
+# country.sort(reverse=True)
+# print(country)
+
+
+#مهمان های شام
+# guests=['Ali','Reza','Sara','Zahra']
+# print(f"we inviting {len(guests)} people to the party")
+
+
+#تمام توابع
+# languages=['java','python','c++','js','php']
+#جابه جا کردن index
+# languages[3]="c#"
+# print(languages)
+
+#appendکردن 
+# languages.append("matlab")
+# print(languages)
+
+#insert کردن
+# languages.insert(2,"c#")
+# print(languages)
+
+#del کردن
+# del languages[4]
+# print(languages)
+
+#pop کردن
+# languages2=languages.pop(0)
+# print(languages2)
+# print(languages)
+
+#remove کردن
+# languages.remove('c++')
+# print(languages)
+
+#sort کردن
+# languages.sort()
+# print(languages)
+
+#sorted کردن
+# languages2=sorted(languages)
+# print(languages2)
+
+#طول لیست
+# print(len(languages))
